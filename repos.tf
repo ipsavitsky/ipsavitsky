89377446a8b10ce77938746d11f5f311c78daf30
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2024-06-03 17:02:05 +0100

    First batch of imported repos
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- repos.tf
+++ repos.tf
@@ -30,3 +30,36 @@ resource "github_repository" "spotistatsR" {
   name       = "spotistatsR"
   visibility = "private"
 }
+
+resource "github_repository" "resume" {
+  name          = "resume"
+  has_downloads = true
+}
+
+resource "github_repository" "simple_shell" {
+  name       = "simple_shell"
+  has_issues = true
+}
+
+
+resource "github_repository" "codewars_solutions" {
+  name                 = "codewars-solutions"
+  vulnerability_alerts = true
+  allow_rebase_merge   = false
+  allow_squash_merge   = false
+}
+
+
+resource "github_repository" "leetcode_solutions" {
+  name = "leetcode-solutions"
+}
+
+resource "github_repository" "cryptobotAPI" {
+  name                 = "cryptobotAPI"
+  has_issues           = true
+  vulnerability_alerts = true
+}
+
+resource "github_repository" "multiprocessor_scheduling_text" {
+  name = "multiprocessor-scheduling-text"
+}
