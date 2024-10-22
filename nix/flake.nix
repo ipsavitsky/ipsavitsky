f0ebbaa46192d63a687e9dc06987bc7720c29366
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2024-10-22 12:57:03 +0100

    Add zen-browser
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,8 +5,10 @@
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     home-manager.url = "github:nix-community/home-manager";
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
   };
-  outputs = { self, nixpkgs, nixos-wsl, home-manager, emacs-overlay, ... }:
+
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, emacs-overlay, zen-browser, ... }@inputs:
   let
     system = "x86_64-linux";
   in {
@@ -21,6 +23,7 @@
         ];
       };
       home-pc = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
         inherit system;
         modules = [
           ./home-pc/configuration.nix
--- nix/home-pc/configuration.nix
+++ nix/home-pc/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
@@ -97,6 +97,8 @@
   environment.systemPackages = with pkgs; [
     telegram-desktop
     alacritty
+  ] ++ [
+    inputs.zen-browser.packages."${pkgs.system}".specific
   ];
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
