38526505a97f9b82bd0bed082696df5f3d8b79d8
Ilya Savitsky <ipsavitsky234@gmail.com>
2024-10-29 07:13:08 +0000

    Add some kitty configuration

--- nix/home-pc/home-manager/kitty/kitty.nix
+++ nix/home-pc/home-manager/kitty/kitty.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.kitty = {
     enable = true;
+    theme = "Dracula";
+    font = {
+      name = "Fira Code";
+    };
+    shellIntegration.enableBashIntegration = true;
   };
 }
