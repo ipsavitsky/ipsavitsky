ef888e091e48646a36b0689429d01ce46476825d
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2024-11-26 14:41:45 +0000

    Remove flake-in-flake, configs as aoutputs of root flake
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- flake.nix
+++ flake.nix
@@ -1,10 +1,33 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      nixos-wsl,
+      home-manager,
+      emacs-overlay,
+      zen-browser,
+    }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -14,12 +37,36 @@
     in
     {
       formatter.${system} = pkgs.nixfmt-rfc-style;
+
       devShells.${system} = {
         default = pkgs.mkShell {
           packages = with pkgs; [
             nil
             terraform-ls
             terraform
+          ];
+        };
+      };
+
+      nixosConfigurations = {
+        hephaestus = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./nix/hosts/hephaestus/configuration.nix
+            nixos-wsl.nixosModules.wsl
+            home-manager.nixosModules.home-manager
+            { nixpkgs.overlays = [ emacs-overlay.overlay ]; }
+          ];
+        };
+        zeus = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          inherit system;
+          modules = [
+            ./nix/hosts/zeus/configuration.nix
+            home-manager.nixosModules.home-manager
+            { nixpkgs.overlays = [ emacs-overlay.overlay ]; }
           ];
         };
       };
