930165f4b0e154255560736492c151c14ca2665e
Ilya Savitsky <ipsavitsky234@gmail.com>
2024-11-28 06:57:20 +0000

    Add a bit of better corfu configuration

--- nix/modules/emacs/emacs-confs/corfu.el
+++ nix/modules/emacs/emacs-confs/corfu.el
@@ -1,9 +1,25 @@
 (use-package corfu
-  :init
-  (global-corfu-mode))
-
-(use-package emacs
   :custom
-  (tab-always-indent 'complete)
-  (text-mode-ispell-word-completion nil)
-  (read-extended-command-predicate #'command-completion-default-include-p))
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.1)
+  (corfu-popupinfo-delay '(0.5 . 0.2))
+  (corfu-preview-current 'insert)
+  (corfu-preselect 'prompt)
+  :bind (:map corfu-map
+              ("TAB"        . corfu-next)
+              ([tab]        . corfu-next)
+              ("S-TAB"      . corfu-previous)
+              ([backtab]    . corfu-previous)
+              ("S-<return>" . corfu-insert)
+              ("RET"        . nil))
+  :init
+  (global-corfu-mode)
+  (corfu-history-mode)
+  (corfu-popupinfo-mode))
+
+;; (use-package cape
+;;   :bind ("C-c f" . cape-file)
+;;   :config
+;;   (advice-add #'eglot-completion-at-point :around #'cape-wrap-nonexclusive))
