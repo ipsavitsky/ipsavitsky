04791d16eb441c6e4478db1037fedefaf71166cc
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-01-15 11:16:23 +0000

    Add nix gitlab_due_date config to demeter
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- nix/hosts/demeter/configuration.nix
+++ nix/hosts/demeter/configuration.nix
@@ -3,7 +3,13 @@
   gitlab_due_date,
   ...
 }:
-
+let
+  gitlab_dd_config_file = builtins.toFile "gitlab_dd_config_file" (
+    builtins.toJSON {
+      base_url = "gitlab.codethink.co.uk/api/v4";
+    }
+  );
+in
 {
   time.timeZone = "Europe/London";
 
@@ -104,22 +110,29 @@
     };
   };
 
-  # systemd.timers."gitlab_due_date" = {
-  #   wantedBy = ["timers.target"];
-  #   timerConfig = {
-  #     OnBootSec = "5m";
-  #     OnUnitActiveSec = "5m";
-  #     Unit = "gitlab_due_date.service";
-  #   };
-  # };
+  systemd.timers."gitlab_due_date" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "5m";
+      OnUnitActiveSec = "5m";
+      Unit = "gitlab_due_date.service";
+    };
+  };
 
-  # systemd.services."gitlab_due_date" = {
-  #   script = "${gitlab_due_date.packages.${pkgs.system}.default}/bin/gitlab_due_date";
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     User = "root";
-  #   };
-  # };
+  systemd.services."gitlab_due_date" = {
+    script = "${
+      gitlab_due_date.packages.${pkgs.system}.default
+    }/bin/gitlab_due_date ${gitlab_dd_config_file}";
+
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+
+    environment = {
+      GITLAB_DD_TOKEN = "";
+    };
+  };
 
   nix.settings = {
     experimental-features = [
