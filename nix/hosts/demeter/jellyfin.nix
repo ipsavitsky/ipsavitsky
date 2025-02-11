edfb48a86e3fa565d65ab7cca41a6913cec0f95c
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-02-11 21:50:50 +0000

    Set cache dir on the ssd

--- nix/hosts/demeter/jellyfin.nix
+++ nix/hosts/demeter/jellyfin.nix
@@ -2,12 +2,14 @@
 {
   systemd.tmpfiles.rules = [
     "d /data/jellyfin 0700 jellyfin jellyfin -"
+    "d /data/jellyfin-cache 0700 jellyfin jellyfin -"
   ];
 
   services.jellyfin = {
     enable = true;
 
     dataDir = "/data/jellyfin";
+    cacheDir = "/data/jellyfin-cache";
     openFirewall = true;
   };
 }
