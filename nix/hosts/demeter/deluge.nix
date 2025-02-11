33c31619cd0ba63cdc7f0901b24db569a5835cb5
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-02-11 11:00:53 +0000

    Add deluge to the configuration
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

---
+++ nix/hosts/demeter/deluge.nix
@@ -0,0 +1,12 @@
+{...}:{
+  systemd.tmpfiles.rules = [
+    "d /data/deluge 0700 deluge deluge -"
+  ];
+
+  services.deluge = {
+    enable = true;
+
+    dataDir = "/data/deluge";
+    openFirewall = true;
+  };
+}
