07f14da6deecbdb5034d2d9fa34011ac84929f81
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-03-19 16:53:18 +0000

    Add zed modules and configuration

--- nix/modules/zed.nix
+++ nix/modules/zed.nix
@@ -8,11 +8,17 @@
     enable = true;
     package = zed.packages."${pkgs.system}".default;
     extensions = [
-      "nix" "rust" "python" "bash"
+      "nix"
+      "rust"
+      "python"
+      "bash"
     ];
     userSettings = {
       telemetry.metrics = false;
-      languages.Nix.language_servers = [ "nil" "!nixd" ];
+      languages.Nix.language_servers = [
+        "nil"
+        "!nixd"
+      ];
       load_direnv = "shell_hook";
       base_keymap = "Emacs";
       show_whitespaces = "all";
