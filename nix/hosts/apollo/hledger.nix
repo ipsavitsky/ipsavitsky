52a14c05d1f04998c81de2bc52a6fcec3999377f
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-04-16 22:11:34 +0100

    Remove the nginx configuration

--- nix/hosts/apollo/hledger.nix
+++ nix/hosts/apollo/hledger.nix
@@ -1,12 +1,18 @@
 _: {
-  services.nginx = {
-    virtualHosts."ledger.savitsky.dev" = {
-      basicAuth = {
-        "ilya" = "changeMeLater";
-      };
-      locations."/".proxyPass = "http://127.0.0.1:9999";
-    };
-  };
+  # services.nginx = {
+  #   virtualHosts."192.168.55.108" = {
+  #     basicAuth = {
+  #       "ilya" = "changeMeLater";
+  #     };
+  #     locations."/".proxyPass = "http://127.0.0.1:9998";
+  #     listen = [
+  #       {
+  #         addr = "0.0.0.0";
+  #         port = 9999;
+  #       }
+  #     ];
+  #   };
+  # };
 
   systemd.tmpfiles.rules = [
     "d /var/lib/hledger-web/ 0700 hledger hledger -"
@@ -18,7 +24,7 @@ _: {
   services.hledger-web = {
     enable = true;
     port = 9999;
-    host = "ledger.savitsky.dev"; # change this later to local dns
+    host = "192.168.55.108"; # change this later to local dns
     allow = "edit";
   };
 }
