0ce52c1b4a7fedc63ea3a86c75890e262ae86a8b
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-04-27 19:17:53 +0100

    Update

--- nix/hosts/apollo/silverbullet.nix
+++ nix/hosts/apollo/silverbullet.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   sops.secrets."silverbullet/environment" = {
     owner = "silverbullet";
@@ -6,6 +6,18 @@
 
   services.silverbullet = {
     enable = true;
+    package = pkgs.silverbullet.overrideAttrs (oldAttrs: rec {
+      version = "0.10.4";
+      src = pkgs.fetchurl {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${version}/silverbullet.js";
+        hash = "sha256-ko1zXfvn0rVY+lp9zTZ71BL41h7AOazooBVeqELP3Ps=";
+      };
+    });
     envFile = config.sops.secrets."silverbullet/environment".path;
+  };
+  # https://github.com/NixOS/nixpkgs/pull/356163
+  systemd.services.silverbullet.serviceConfig = {
+    Environment = "DENO_DIR=/var/cache/silverbullet/deno";
+    CacheDirectory = "silverbullet";
   };
 }
