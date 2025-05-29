ce406ab744972aa3eecb504814c0219f56ded03d
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-05-29 23:56:51 +0100

    Add renovate

--- nix/hosts/apollo/renovate.nix
+++ nix/hosts/apollo/renovate.nix
@@ -1,4 +1,30 @@
-{ ... }:
+{ pkgs, config, ... }:
 {
-  services.renovate.enable = true;
+  sops.secrets."renovate/github_com" = {};
+  services.renovate = {
+    enable = true;
+    schedule = "daily";
+    validateSettings = true;
+    runtimePackages = with pkgs; [
+      go
+      config.nix.package
+      git
+      openssh
+    ];
+    settings = {
+      autodiscover = true;
+      autodiscoverTopics = [ "managed-by-renovate" ];
+      labels = [
+        "dependencies"
+        "renovate"
+      ];
+      nix.enabled = true;
+      lockFileMaintenance.enabled = true;
+      username = "Ilya's renovate bot";
+      gitAuthor = "Ilya's renovate bot <renovate@savitsky.invalid>";
+    };
+    credentials = {
+      RENOVATE_TOKEN = config.sops.secrets."renovate/github_com".path;
+    };
+  };
 }
