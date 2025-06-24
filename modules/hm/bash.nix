cd8f4d027d12bdce89c07f85e7e75722bbfc9fdf
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-06-24 14:57:50 +0100

    Add ai secrets to bash envvar config

--- modules/hm/bash.nix
+++ modules/hm/bash.nix
@@ -1,8 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }: # Added 'config' to the arguments
+
 {
+  sops.secrets."bash/api_keys.sh" = {
+    sopsFile = ../../secrets/hm.yaml;
+  };
+
   programs.bash = {
     enable = true;
     package = pkgs.bash;
     enableVteIntegration = true;
+    initExtra = ''
+      source "${config.sops.secrets."bash/api_keys.sh".path}"
+    '';
   };
 }
--- modules/hm/mods.nix
+++ modules/hm/mods.nix
@@ -29,6 +29,41 @@
             };
           };
         };
+        anthropic = {
+          base-url = "https://api.anthropic.com/v1";
+          api-key-env = "ANTHROPIC_API_KEY";
+          models = {
+            "claude-3-7-sonnet-latest" = {
+              aliases = [
+                "claude"
+              ];
+              max-input-chars = 680000;
+            };
+          };
+        };
+        google = {
+          api-key-env = "GOOGLE_API_KEY";
+          models = {
+            "gemini-2.5-flash-latest" = {
+              aliases = [
+                "gemini"
+              ];
+              max-input-chars = 4194304;
+            };
+          };
+        };
+        mistral = {
+          base-url = "https://api.mistral.ai/v1";
+          api-key-env = "MISTRAL_API_KEY";
+          models = {
+            "mistral-large-latest" = {
+              aliases = [
+                "mistral"
+              ];
+              max-input-chars = 384000;
+            };
+          };
+        };
       };
     };
   };
