ba5358dcf689c4b426930e7ac04b31c54a62e80c
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-07-21 09:03:30 +0100

    dracula -> nord for the theme

--- modules/hm/stylix.nix
+++ modules/hm/stylix.nix
@@ -11,7 +11,7 @@
 
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
     image = config.lib.stylix.pixel "base01";
     fonts = {
       serif = config.stylix.fonts.sansSerif;
