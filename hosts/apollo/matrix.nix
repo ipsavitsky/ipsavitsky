9be10677527edd3f3c55ec1e802b42b32cbc48fd
Ilya Savitsky <ipsavitsky234@gmail.com>
2025-09-11 23:37:23 +0100

    set up the conduit server

--- hosts/apollo/matrix.nix
+++ hosts/apollo/matrix.nix
@@ -5,6 +5,15 @@ _: {
       global = {
         server_name = "matrix.savitsky.dev";
         enable_lightning_bolt = false;
+        # trusted_servers = [
+          # "matrix.org"
+          # "nixos.org"
+          # "chat.lfx.linuxfoundation.org"
+        # ];
+        allow_encryption = true;
+        allow_federation = true;
+        allow_registration = true;
+        database_backend = "rocksdb";
       };
     };
   };
