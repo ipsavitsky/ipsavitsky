0e83b5fe836825781b487716a3dbb5f5e18b8b44
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-10-10 12:23:58 +0100

    set silverbullet to track unstable

--- hosts/apollo/silverbullet.nix
+++ hosts/apollo/silverbullet.nix
@@ -1,23 +1,21 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  inputs,
+  config,
+  ...
+}:
 {
   sops.secrets."silverbullet/environment" = {
     owner = "silverbullet";
   };
 
-  services.silverbullet = {
-    enable = true;
-    package = pkgs.silverbullet.overrideAttrs (_oldAttrs: rec {
-      version = "0.10.4";
-      src = pkgs.fetchurl {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${version}/silverbullet.js";
-        hash = "sha256-ko1zXfvn0rVY+lp9zTZ71BL41h7AOazooBVeqELP3Ps=";
-      };
-    });
-    envFile = config.sops.secrets."silverbullet/environment".path;
-  };
-  # https://github.com/NixOS/nixpkgs/pull/356163
-  systemd.services.silverbullet.serviceConfig = {
-    Environment = "DENO_DIR=/var/cache/silverbullet/deno";
-    CacheDirectory = "silverbullet";
-  };
+  services.silverbullet =
+    let
+      unstable_pkgs = import inputs.nixpkgs-unstable { inherit (pkgs) system; };
+    in
+    {
+      enable = true;
+      package = unstable_pkgs.silverbullet;
+      envFile = config.sops.secrets."silverbullet/environment".path;
+    };
 }
