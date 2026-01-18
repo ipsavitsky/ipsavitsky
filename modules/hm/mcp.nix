d93438594beacb328854e733109abd14159c1448
Ilya Savitsky <ipsavitsky234@gmail.com>
2026-01-18 22:59:39 +0000

    add github mcp

--- modules/hm/mcp.nix
+++ modules/hm/mcp.nix
@@ -10,6 +10,10 @@
     sopsFile = ../../secrets/hm.yaml;
   };
 
+  sops.secrets."renovate/github_com" = {
+    sopsFile = ../../secrets/demeter.yaml;
+  };
+
   programs.mcp = {
     enable = true;
     servers = {
@@ -26,6 +30,12 @@
         url = "https://mcp.context7.com/mcp";
         headers = {
           CONTEXT7_API_KEY = "{file:${config.sops.secrets."mcp/context7_token".path}}";
+        };
+      };
+      github = {
+        url = "https://api.githubcopilot.com/mcp/x/repos/readonly";
+        headers = {
+          Authorization = "Bearer {file:${config.sops.secrets."renovate/github_com".path}}";
         };
       };
     };
